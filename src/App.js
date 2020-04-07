f705971a7bd0c6d20af250adcc3317f01bc0da53
anasansari157 <anasansari157@gmail.com>
2020-04-07 18:48:26 +0530

    Add Sub Page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Add from './components/Add'
+import Sub from './components/Sub'
 
 function App() {
 	return (
 		<div className="App">
 			<Home />
 
-			<Route path="/add" component={Add} />
+      <Route path="/add" component={Add} />
+      <Route path="/sub" component={Sub} />
+      
 		</div>
 	);
 }
---
+++ src/components/Sub.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Sub extends React.Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>Welcome to Subtract Page</h1>
+     );
+  }
+}
+ 
+export default Sub;
