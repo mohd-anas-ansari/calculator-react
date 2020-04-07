8395118f476fcf2cfbbc358d09963be766d9be12
anasansari157 <anasansari157@gmail.com>
2020-04-07 18:04:35 +0530

    Add main-page structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Adder</h1>
+      <div className='buttons-container'>
+      <button>Add</button>
+      <button>Subtract</button>
+      <button>Multiply</button>
+      <button>Divide</button>
+      </div>
     </div>
   );
 }
